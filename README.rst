40ab71af75a1f8cdedc2feb1340b627a63105192
Andrew Bentley <andrew.t.bentley@gmail.com>
2016-07-03 14:06:52 +1000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Postgres Elastic File System
+============================
+
+Store a Postgres database on an Amazon EFS and link to different Postgres instances.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import re
+from setuptools import setup
+
+
+def read(path):
+    with open(os.path.join(os.path.dirname(__file__), path), 'r') as f:
+        data = f.read()
+    return data.strip()
+
+
+_version_re = re.compile(r'\s*__version__\s*=\s*\'(.*)\'\s*')
+version = _version_re.findall(read('pefs.py'))[0]
+
+
+install_requires = read('requirements.txt').split('\n')
+test_requires = read('build-requirements.txt').split('\n')
+test_requires.extend(install_requires)
+
+setup(
+    name='postgres-efs',
+    version=version,
+    url='http://github.com/atbentley/postgres-efs/',
+    license='MIT',
+    author='Andrew Bentley',
+    author_email='andrew.t.bentley@gmail.com',
+    description='Store Postgres databases on Amazon elastic file systems.',
+    long_description=read('README.rst'),
+    py_modules=['pefs'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=install_requires,
+    tests_require=test_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
+
+
